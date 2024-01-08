10b03bc02a9dd33042f0c90d4f938008c8f47bc0
OzoneMotion <seovelaz@gmail.com>
2024-01-08 16:54:36 -0600

    el ejercicio es este

---
+++ ej1.js
@@ -0,0 +1,83 @@
+//Ejercicio para esta sesion
+ 
+/*Declarar 10 variables de cada tipo de dato sin tomar en cuenta objetos, arrays y null
+Pueden variar entre las palabras reservadas var, let y const
+      -number
+      -string
+      -boolean */
+var firstNumber=true;
+let esDeportista=true;
+let cantaBien=true;
+let esGimnasta=false;
+let esIngeniero=true;
+let isRaining=false;
+let isSunny=true;
+let areYouAlive=true;
+let newPhone=true;
+let android=true;
+
+var verduras = 25;
+const secondNumber = 25;
+let birthYear = 1998;
+let añoIngresoBootcamp = 2023;
+let age=25;
+let kpopAlbums=8;
+let backpacks=2;
+let pets=2;
+let perfumes=6;
+let skincareSteps=4;
+
+var favArtist = "Ryuichi Sakamoto";
+var Book = "1Q84"
+let userName= "Juanito";
+let apellidoPaterno="García";
+let comidaFavorita = "Espinacas";
+let skincareBrand = "Vichy";
+let domicilioLaboral = "20 noviembre";
+let salarioMensual = "1000 euros";
+let hobbies= "plantas, videojuegos, leer, música";
+let juegofavorito = "Final Fantasy VII remake"
+
+/*Declarar variables numericas y realizar las operaciones aritmeticas basicas
+      -suma
+      -resta
+      -multiplicacion
+      -division*/
+let primerNumero = 5;
+let segundoNumero= 3;
+
+let sum = primerNumero+segundoNumero
+let minus = primerNumero-segundoNumero
+let by = primerNumero*segundoNumero
+let div = primerNumero/segundoNumero
+ 
+/*Declarar variables que concatenen cadenas de texto
+      -5 mensajes diferentes en pantalla, sin limite de palabras.*/
+let nombreUsuario = "Juan";
+let apellidoMaterno ="Ramírez";
+let estadoResidencia = "Jalisco";
+let juegoOnlineFavorito = "Overwatch"
+let frutaFavorita = "naranja";
+let overwatchFavCharacter = "Symmetra"
+
+console.log("Hola, tu nombre es "+nombreUsuario)
+console.log("y tu apellido materno "+apellidoMaterno)
+console.log("Guadalajara se encuentra en el estado de "+estadoResidencia)
+console.log("Mi videojuego online favorito es "+juegoOnlineFavorito+" y mi personaje favorito es "+overwatchFavCharacter)
+console.log("Y mi fruta favorita es la "+frutaFavorita)
+ 
+/*Declarar 5 variables y convertirlas a otro tipo de dato*/
+let cancionesDescargadas=1150;
+let posesionCelular= false;
+let edad=47;
+let mascotasEnHogar= 3;
+let numeroRecamarasHogar= 2;
+
+let cancionesString = String(cancionesDescargadas);
+let celularInt = Number(posesionCelular);
+let edadString = String(edad);
+let mascotaString = String(mascotasEnHogar);
+let recamarasString = String(numeroRecamarasHogar);
+ 
+/*CONSIDERACIONES: poner en practica las mejores practicas para declaracion de variables.
+Imprimir los resultados en pantalla con un console.log()*/
